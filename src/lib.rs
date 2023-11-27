ef32549b66c3eacc9bfa17416c1bcdce81ee7cf3
Marco Inacio <m@marcoinacio.com>
2023-11-27 20:40:50 +0000

    update Axum to 0.7

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
 mod llm;
 mod routes;
+use tokio::net::TcpListener;
 
 pub async fn run() -> Result<(), Box<dyn std::error::Error>> {
     let app = routes::create_routes().await?;
-    let bind_addr = &"0.0.0.0:51005"
-        .parse()
-        .map_err(|e| format!("Failed to parse address: {}", e))?;
+    let bind_addr = &"0.0.0.0:51005";
     println!("Listening on: http://localhost:51005");
-    axum::Server::bind(bind_addr)
-        .serve(app.into_make_service())
+    let listener = TcpListener::bind(bind_addr)
+        .await
+        .map_err(|e| format!("Failed to parse address: {}", e))?;
+    axum::serve(listener, app.into_make_service())
         .await
         .map_err(|e| format!("Server error: {}", e))?;
     Ok(())
